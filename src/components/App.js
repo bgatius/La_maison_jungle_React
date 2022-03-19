7d9831b1398e64bc3ea819af4e6b4f2cdbac1773
benjamin gatius <benjamin.gatius@gmail.com>
2022-03-19 13:52:23 +0100

    init la maison jungle react

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import Banner from "./Banner";
+import Cart from "./Cart";
+import ShoppingList from "./ShoppingList";
+
+function App() {
+	return <div> <Banner /> <Cart /> <ShoppingList /> </div>
+}
+
+export default App;
